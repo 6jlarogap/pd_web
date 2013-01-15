04d06d96181060288076bf2e73932e45b4d1d134
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-01-14 20:15:15 -0500

    fabric

--- pd/pd/wsgi.py
+++ pd/pd/wsgi.py
@@ -17,6 +17,9 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pd.settings")
 
+activate_this = os.path.join(os.path.dirname(__file__), '..', '..', 'ENV', 'bin', 'activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
