be5ec3814f8392adb486b0c91f745db3d45d6999
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-01-14 19:45:33 -0500

    fabric

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,32 @@ from fabric.api import *
 
 env.hosts = ['youmemory.org']
 
+def sshagent_run(cmd):
+    """
+    Helper function.
+    Runs a command with SSH agent forwarding enabled.
+
+    Note:: Fabric (and paramiko) can't forward your SSH agent.
+    This helper uses your system's ssh to do so.
+    """
+
+    for h in env.hosts:
+        try:
+            # catch the port number to pass to ssh
+            host, port = h.split(':')
+            try:
+                # catch username too
+                user, real_host = host.split('@')
+                local('ssh -p %s -A %s -l %s "%s"' % (port, real_host, user, cmd))
+            except ValueError:
+                local('ssh -p %s -A %s "%s"' % (port, host, cmd))
+        except ValueError:
+            local('ssh -A %s "%s"' % (h, cmd))
+
 def deploy():
     local('git push')
-    with cd('/home/www-data/django/pd_web/'):
-        run('sudo -u www-data git pull && sudo /etc/init.d/apache2 reload')
+    local('git push')
+
+    local('ssh-add')
+    sshagent_run('cd /home/www-data/django/pd_web/ && git pull')
+    run('sudo /etc/init.d/apache2 reload')
