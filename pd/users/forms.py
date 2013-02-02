cb77cbd6fe997ed48655755c08043ab2ac0d3dd1
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-02-01 19:11:18 -0500

    reg auto-login

--- pd/users/forms.py
+++ pd/users/forms.py
@@ -7,9 +7,12 @@ from django.utils.translation import ugettext_lazy as _
 from users.models import Profile, ProfileLORU, Org
 
 
-class RegisterForm(forms.Form):
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name',]
+
     username = forms.SlugField(label=_(u"Имя пользователя"))
-    email = forms.EmailField()
     password1 = forms.CharField(label=_(u"Пароль"), widget=forms.PasswordInput())
     password2 = forms.CharField(label=_(u"Пароль (повторите)"), widget=forms.PasswordInput())
 
@@ -29,6 +32,10 @@ class RegisterForm(forms.Form):
             self.cleaned_data['email'],
             self.cleaned_data['password1']
         )
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.is_active = True
+        user.save()
         Profile.objects.create(user=user)
         return user
 
