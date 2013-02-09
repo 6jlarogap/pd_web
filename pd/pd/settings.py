bed5a58c8a1d403ebb8eb1e06e3b2f3335ba8506
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-02-09 13:21:51 -0500

    logging data

--- pd/pd/settings.py
+++ pd/pd/settings.py
@@ -23,14 +23,11 @@ DATABASES = {
     },
     'fias': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pd',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'NAME': 'fias',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        'OPTIONS': {
-            'autocommit': True,
-        },
     }
 }
 
