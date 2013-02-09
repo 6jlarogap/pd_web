c8a4145c54552e4f14211330b01b7bfcaa231b83
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-02-08 22:47:31 -0500

    doverennosti

--- pd/users/admin.py
+++ pd/users/admin.py
@@ -3,18 +3,18 @@ from django.contrib import admin
 from users.models import Profile, Org, ProfileLORU, Dover
 
 
-class ProfileAdmin(admin.ModelAdmin):
-    inlines = [AgentDoverInline, ]
-
-admin.site.register(Profile, ProfileAdmin)
+class AgentDoverInline(admin.TabularInline):
+    model = Dover
+    can_delete = False
 
 class ProfileLORUInline(admin.TabularInline):
     model = ProfileLORU
     fk_name = 'ugh'
 
-class AgentDoverInline(admin.TabularInline):
-    model = Dover
-    can_delete = False
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [AgentDoverInline, ]
+
+admin.site.register(Profile, ProfileAdmin)
 
 class OrgAdmin(admin.ModelAdmin):
     inlines = [ProfileLORUInline, ]
