54c15b82b043fa388245391cc3a3226849ac9168
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-02-13 22:34:06 -0500

    product types

--- pd/orders/models.py
+++ pd/orders/models.py
@@ -6,10 +6,16 @@ from users.models import Org
 
 
 class Product(models.Model):
+    PRODUCT_BURIAL = 'burial'
+    PRODUCT_TYPES = (
+        (PRODUCT_BURIAL, _(u"Захоронение")),
+    )
+
     loru = models.ForeignKey(Org, limit_choices_to={'type': Org.PROFILE_LORU}, null=True, verbose_name=_(u"ЛОРУ"))
     name = models.CharField(_(u"Название"), max_length=255)
     measure = models.CharField(_(u"Ед. изм."), max_length=255, default=_(u"шт"))
     price = models.DecimalField(_(u"Цена"), max_digits=20, decimal_places=2)
+    ptype = models.CharField(_(u"Тип"), max_length=255, choices=PRODUCT_TYPES, null=True, blank=True)
 
     class Meta:
         verbose_name = _(u"Товар")
@@ -17,6 +23,9 @@ class Product(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    def is_burial(self):
+        return self.ptype == self.PRODUCT_BURIAL
 
 class Order(models.Model):
     loru = models.ForeignKey(Org, limit_choices_to={'type': Org.PROFILE_LORU}, null=True, verbose_name=_(u"ЛОРУ"))
