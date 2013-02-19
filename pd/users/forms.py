902b594d27408ced0a7ab2296e7b80f1798f7b4f
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-02-19 16:39:39 -0500

    org email fix

--- pd/users/forms.py
+++ pd/users/forms.py
@@ -54,7 +54,7 @@ class ProfileForm(forms.ModelForm):
     org_full_name = forms.CharField(label=_(u"Полное название организации"), required=False)
     org_inn = forms.CharField(label=_(u"ИНН организации"))
     org_director = forms.CharField(label=_(u"Директор"), required=False)
-    org_email = forms.EmailField(label=_(u"Email"))
+    org_email = forms.EmailField(label=_(u"Email"), required=False)
 
     class Meta:
         model = Profile
