825a26c318209ae0f2bea559f0b77bd47899d7c8
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2013-03-06 21:32:37 -0500

    sentry fix

--- pd/burials/burials_views.py
+++ pd/burials/burials_views.py
@@ -239,13 +239,13 @@ class BurialsListView(ListView):
             if form.cleaned_data['responsible']:
                 burials = burials.filter(place__responsible__last_name__icontains=form.cleaned_data['responsible'])
             if form.cleaned_data['cemetery']:
-                burials = burials.filter(place__cemetery__name__icontains=form.cleaned_data['cemetery'])
+                burials = burials.filter(cemetery__name__icontains=form.cleaned_data['cemetery'])
             if form.cleaned_data['area']:
-                burials = burials.filter(place__area__name__icontains=form.cleaned_data['area'])
+                burials = burials.filter(area__name__icontains=form.cleaned_data['area'])
             if form.cleaned_data['row']:
-                burials = burials.filter(place__row=form.cleaned_data['row'])
+                burials = burials.filter(row=form.cleaned_data['row'])
             if form.cleaned_data['place']:
-                burials = burials.filter(place__seat=form.cleaned_data['seat'])
+                burials = burials.filter(place=form.cleaned_data['place'])
             if form.cleaned_data['no_last_name']:
                 burials = burials.filter(Q(deadman__last_name='') | Q(deadman__last_name__isnull=True))
             if form.cleaned_data['no_responsible']:
