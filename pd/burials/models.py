6fd629766b69e468f644f6b2d22f3c5013b4523b
Eugene Suprun <suprune20@gmail.com>
2013-04-29 17:23:25 +0300

    card 284

--- pd/burials/models.py
+++ pd/burials/models.py
@@ -328,10 +328,6 @@ class Burial(models.Model):
     def is_ugh(self):
         return self.is_ugh_only() or self.is_archive()
 
-    # При этих условиях прячем документы от loru
-    def hide_documents(self):
-        return self.is_declined() or self.is_draft() or self.is_backed() or self.is_ready()
-
     def can_approve(self):
         if self.is_ugh():
             return False
