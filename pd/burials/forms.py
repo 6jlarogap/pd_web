012949ffd230da9eacd9785cdfd357e9326de465
Eugene Suprun <suprune20@gmail.com>
2013-06-02 16:13:41 +0300

    c 320, prevent closing archive burial without account number

--- pd/burials/forms.py
+++ pd/burials/forms.py
@@ -810,8 +810,11 @@ class BurialCommitForm(BurialForm):
             if self.cleaned_data.get('account_number') and self.cleaned_data.get('fact_date'):
                 acc_number = self.cleaned_data.get('account_number')
                 fact_date  = self.cleaned_data.get('fact_date')
-                if len(acc_number) < 4 or int(acc_number[:4]) != fact_date.year:
-                    msg = _(u"Номер в книге учета должен быть: ГГГГнн...н (год фактической даты, номер)")
+                msg = _(u"Номер в книге учета должен быть: ГГГГнн...н (год фактической даты, номер)")
+                try:
+                    if len(acc_number) < 4 or int(acc_number[:4]) != fact_date.year:
+                        raise forms.ValidationError(msg)
+                except ValueError:
                     raise forms.ValidationError(msg)
 
         place_number = self.cleaned_data.get('place_number') or ''
@@ -826,6 +829,10 @@ class BurialCommitForm(BurialForm):
             elif self.request.REQUEST.get('ready'):
                 msg = _(u"Не указано место для закрытого участка. Нельзя отправлять на согласование")
                 raise forms.ValidationError(msg)
+
+        if not self.cleaned_data.get('account_number') and (self.instance.is_archive() or self.request.REQUEST.get('archive')):
+            msg = _(u"Нельзя закрывать архивное захоронение без указания его номера в книге учета")
+            raise forms.ValidationError(msg)
 
         cemetery = self.cleaned_data.get('cemetery')
         today = datetime.date.today()
