62be64816320335c88a2563720a8f67a125906c7
Eugene Suprun <suprune20@gmail.com>
2013-06-11 19:33:09 +0300

    c.384, first, middle names autocomplete

--- pd/persons/urls.py
+++ pd/persons/urls.py
@@ -6,5 +6,7 @@ from django.conf import settings
 urlpatterns = patterns('persons.views',
     url(r'^autocomplete/fio/', 'autocomplete_fio', name='autocomplete_fio'),
     url(r'^autocomplete/alive/', 'autocomplete_alive', name='autocomplete_alive'),
+    url(r'^autocomplete/firstname/', 'autocomplete_first_name', name='autocomplete_first_name'),
+    url(r'^autocomplete/middlename/', 'autocomplete_middle_name', name='autocomplete_middle_name'),
     url(r'^autocomplete/docsources/', 'autocomplete_docsources', name='autocomplete_docsources'),
 )
--- pd/persons/views.py
+++ pd/persons/views.py
@@ -4,7 +4,7 @@ from django.db.models.query_utils import Q
 from django.http import HttpResponse
 from django.views.generic.base import View
 
-from persons.models import DeadPerson, AlivePerson, DocumentSource
+from persons.models import DeadPerson, AlivePerson, BasePerson, DocumentSource
 
 
 class AutocompleteFIO(View):
@@ -24,6 +24,22 @@ class AutocompleteFIO(View):
         return HttpResponse(json.dumps([{'value': unicode(c)} for c in persons[:20]]), mimetype='text/javascript')
 
 autocomplete_fio = AutocompleteFIO.as_view()
+
+class AutocompleteFirstName(View):
+    def get(self, request, *args, **kwargs):
+        query = request.GET['query']
+        first_names = BasePerson.objects.filter(first_name__istartswith=query).order_by('first_name').distinct('first_name')
+        return HttpResponse(json.dumps([{'value': c.first_name} for c in first_names[:20]]), mimetype='text/javascript')
+
+autocomplete_first_name = AutocompleteFirstName.as_view()
+
+class AutocompleteMiddleName(View):
+    def get(self, request, *args, **kwargs):
+        query = request.GET['query']
+        middle_names = BasePerson.objects.filter(middle_name__istartswith=query).order_by('middle_name').distinct('middle_name')
+        return HttpResponse(json.dumps([{'value': c.middle_name} for c in middle_names[:20]]), mimetype='text/javascript')
+
+autocomplete_middle_name = AutocompleteMiddleName.as_view()
 
 class AutocompleteAlive(View):
     def get(self, request, *args, **kwargs):
