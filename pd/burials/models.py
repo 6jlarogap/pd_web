4d901aef22c4d8b7b6075e95f692704d464350ea
Eugene Suprun <suprune20@gmail.com>
2013-07-05 14:34:15 +0300

    for sync

--- pd/burials/models.py
+++ pd/burials/models.py
@@ -527,6 +527,9 @@ class Burial(models.Model):
                 if not old_place or not old_place.pk or not old_place.burial_count(): # and FROM old and populated
                     pass # do not touch anything
                 else: # from new
+                    # TODO: ответить на вопрос? А сработает ли это когда-нибудь? Без ProtectedError ?
+                    #       если в месте есть захоронения, то на него есть ссылки из таблицы Burial.
+                    #       Всегда будет ProtectedError.
                     try:
                         old_place.delete() # deleting old
                     except (AttributeError, ProtectedError):
