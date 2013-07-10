5eea78b6d93528e8f7cedeef07daafd5423b0afa
Eugene Suprun <suprune20@gmail.com>
2013-07-10 19:47:57 +0300

    c.457, лишние поля из форм пользователя (auth) и профиля

--- pd/users/forms.py
+++ pd/users/forms.py
@@ -101,11 +101,11 @@ class ProfileForm(ChildrenJSONMixin, forms.ModelForm):
         return obj
 
 class UserProfileForm(ChildrenJSONMixin, forms.ModelForm):
-    region = forms.ModelChoiceField(label=_(u"Регион"), queryset=FIAS_REGIONS, required=False)
+    # region = forms.ModelChoiceField(label=_(u"Регион"), queryset=FIAS_REGIONS, required=False)
 
     class Meta:
         model = Profile
-        exclude = ['org', 'is_agent', 'region_fias', 'user']
+        exclude = ['org', 'is_agent', 'region_fias', 'country', 'user']
 
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
@@ -114,23 +114,23 @@ class UserProfileForm(ChildrenJSONMixin, forms.ModelForm):
             Q(ugh__loru_list__loru=self.instance.org) |
             Q(ugh=self.instance.org)
         ).distinct()    
-        if self.instance.region_fias:
-            self.initial['region'] = self.instance.get_region()
+        #if self.instance.region_fias:
+            #self.initial['region'] = self.instance.get_region()
 
-    def save(self, commit=True, *args, **kwargs):
-        obj = super(UserProfileForm, self).save(commit=False, *args, **kwargs)
-        if self.cleaned_data['region']:
-            obj.region_fias = self.cleaned_data['region'].aoguid
-        if commit:
-            obj.save()
-        return obj
+    #def save(self, commit=True, *args, **kwargs):
+        #obj = super(UserProfileForm, self).save(commit=False, *args, **kwargs)
+        #if self.cleaned_data['region']:
+            #obj.region_fias = self.cleaned_data['region'].aoguid
+        #if commit:
+            #obj.save()
+        #return obj
 
 class UserDataForm(forms.ModelForm):
     is_agent = forms.BooleanField(label=_(u"Агент"), required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'is_active' ,]
+        fields = ['username', 'email', 'is_active' ,]
 
     def __init__(self, *args, **kwargs):
         super(UserDataForm, self).__init__(*args, **kwargs)
