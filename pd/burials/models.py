f04437dd12179a17a0d44e418697d864af16c8c8
Eugene Suprun <suprune20@gmail.com>
2013-07-22 15:38:38 +0300

    cemetery time_begin, time_end not mandatory

--- pd/burials/models.py
+++ pd/burials/models.py
@@ -33,8 +33,8 @@ class Cemetery(models.Model):
     )
 
     name = models.CharField(_(u"Название"), max_length=255)
-    time_begin = models.TimeField(_(u"Начало работы"))
-    time_end = models.TimeField(_(u"Окончание работы"))
+    time_begin = models.TimeField(_(u"Начало работы"), null=True, blank=True)
+    time_end = models.TimeField(_(u"Окончание работы"), null=True, blank=True)
     places_algo = models.CharField(_(u"Расстановка номеров мест"), max_length=255, choices=PLACE_TYPES, default=PLACE_MANUAL)
     time_slots = models.TextField(_(u"Время для захоронения"), default='', blank=True,
                                   help_text=_(u'В формате ЧЧ:ММ, по одному на строку'))
