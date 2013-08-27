1262fd218a0a3f2f97f60cc065fb09cd9272f299
Eugene Suprun <suprune20@gmail.com>
2013-08-27 16:50:27 +0300

    fix 4

--- pd/logs/models.py
+++ pd/logs/models.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
+from django.db.models.loading import get_model
 
 class Log(models.Model):
     """
@@ -22,14 +23,12 @@ class Log(models.Model):
         Показываем в таблице действий пользователей: что за объект + ссылка
         """
 
-        # Делаем импорт здесь во избежание перекрестных ссылок
-        # между моделями приложений, которые все используют
-        # методы этого модуля
- 
-        from burials.models import Cemetery
-        from orders.models import Order
-        
         model_name = self.ct.model_class()._meta.object_name
+        app_name = self.ct.model_class()._meta.app_label
+        # Во избежание перекрестных ссылок между модулями
+        # различных приложений, применяющих здешние функции,
+        # не импортируем модели, а получаем их так:
+        Model = get_model(app_name, model_name)
         obj_id = self.obj_id if self.obj_id else ''
         if model_name == 'Burial':
             ref = reverse('view_burial', args=[obj_id])
@@ -37,15 +36,15 @@ class Log(models.Model):
         elif model_name == 'Order':
             ref = reverse('order_edit', args=[obj_id])
             try:
-                loru_number = Order.objects.get(pk=obj_id).loru_number
-            except Order.DoesNotExist:
+                loru_number = Model.objects.get(pk=obj_id).loru_number
+            except Model.DoesNotExist:
                 loru_number = ''
             result = _(u"Заказ <a href='%s'>%s</a>") % (ref, loru_number, )
         elif model_name == 'Cemetery':
             ref = reverse('manage_cemeteries_edit', args=[obj_id])
             try:
-                cemetery = Cemetery.objects.get(pk=obj_id).name
-            except Cemetery.DoesNotExist:
+                cemetery = Model.objects.get(pk=obj_id).name
+            except Model.DoesNotExist:
                 cemetery = ''
             result = _(u"Кладбище <a href='%s'>%s</a>") % (ref, cemetery, )
         elif model_name == 'Org':
