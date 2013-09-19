c8b378108c8509b123c8ad48c43ad2a7c6756696
Eugene Suprun <suprune20@gmail.com>
2013-09-20 00:17:36 +0300

    fix 13-09-20-a

--- pd/persons/models.py
+++ pd/persons/models.py
@@ -249,17 +249,6 @@ class DeathCertificate(BaseModel):
         self.series = self.series.upper()
         super(DeathCertificate, self).save(*args, **kwargs)
 
-    def delete(self):
-        try:
-            self.deathcertificatescan.delete()
-        except DeathCertificateScan.DoesNotExist:
-            pass
-        else:
-            try:
-                super(DeathCertificate, self).delete()
-            except ProtectedError:
-                pass
-
 class DeathCertificateScan(Files):
     """
     Файлы-сканы свидетельства о смерти, по одному на СоС
