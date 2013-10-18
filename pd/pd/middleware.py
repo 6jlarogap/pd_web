5a96797062c4742cf9def2cf74bd1b3d5a2d7392
Eugene Suprun <suprune20@gmail.com>
2013-10-18 15:40:00 +0300

    remove debugging ops

--- pd/pd/middleware.py
+++ pd/pd/middleware.py
@@ -15,7 +15,6 @@ class LoginRequiredMiddleware:
     def process_request(self, request):
         if not request.user.is_authenticated():
             path = request.path_info.lstrip('/')
-            print path
             if not any(m.match(path) for m in exempt_urls):
                 next = '' if not path or exempt_urls[0].match(path) else '?next='+request.build_absolute_uri()
                 return HttpResponseRedirect(settings.LOGIN_URL+next)
