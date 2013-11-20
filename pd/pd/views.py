6f38d8031d41f825011f919ee69398716bd7a46f
Eugene Suprun <suprune20@gmail.com>
2013-11-20 19:15:23 +0300

    c.635, + исправлено, чтоб можно было разные базы прикреплять к проекту

--- pd/pd/views.py
+++ pd/pd/views.py
@@ -97,13 +97,3 @@ def media_xsendfile(request, path, document_root):
         #
         from django.views.static import serve
         return serve(request, path, document_root)
-
-def skip_unreadable_post(record):
-    """
-     Filter out UnreadablePostError (raised when a user cancels an upload) from the admin emails
-    """
-    if record.exc_info:
-        exc_type, exc_value = record.exc_info[:2]
-        if isinstance(exc_value, UnreadablePostError):
-            return False
-    return True
