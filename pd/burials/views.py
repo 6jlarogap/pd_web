04651eead147507ff656a5ab61334f14370e07c5
SK <korotkin.s@gmail.com>
2014-02-12 18:59:21 +0300

    upd

--- pd/burials/views.py
+++ pd/burials/views.py
@@ -107,28 +107,33 @@ class CemeteryViewSet(viewsets.ModelViewSet):
         return  Cemetery.objects.filter(ugh=self.request.user.profile.org).all()
 
     
+    def check_semetery_name(self, request, pk=None):
+        name = request.DATA.get('name')
+        if not name:
+            return {"__all__":[u"Название кладбища обязательно",]}
+        qs = self.get_queryset().filter(ugh=self.request.user.profile.org, name__iexact= name)
+        if pk:
+            qs = qs.exclude(pk=pk)
+        if qs.exists():
+            return {"__all__":[u"Кладбище с таким названием уже существует",]}
+
+    
     def create(self, request, *args, **kwargs):
         """
         Add "unique together" check in parent class 
         """
-        name = request.DATA.get('name','').upper()
-        if self.get_queryset().filter(ugh=self.request.user.profile.org).extra(where=['upper(name)=%s'], params=[name]).exists():
-            data = {"__all__":[u"Кладбище с таким названием уже существует",]}
+        data = self.check_semetery_name(request)
+        if data:
             return Response(status=400, data=data)
         return super(CemeteryViewSet, self).create(request, *args, **kwargs)
-    
-    
+
+
     def update(self, request, *args, **kwargs):
         try:
             pk = int(request.DATA.get('id'))
         except:
             return Response(status=400)
-        name = request.DATA.get('name')
-        data = None
-        if not name:
-            data = {"__all__":[u"Название кладбища обязательно",]}
-        elif self.get_queryset().exclude(pk=pk).filter(ugh=self.request.user.profile.org, name__iexact= name).exists():
-            data = {"__all__":[u"Кладбище с таким названием уже существует",]}
+        data = self.check_semetery_name(request, pk)
         if data:
             return Response(status=400, data=data)
         return super(CemeteryViewSet, self).update(request, *args, **kwargs)
