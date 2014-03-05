ad5e6ab06178bf50407629b148b0ea057ffffe42
Eugene Suprun <suprune20@gmail.com>
2014-03-05 13:32:34 +0300

    fill wallets for existing lorus, ughs, rates for ughs

---
+++ pd/users/management/commands/fill_wallets_rates.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from django.core.management.base import BaseCommand
+from users.models import Org
+from billing.models import Currency
+
+class Command(BaseCommand):
+    args = '<Currecy_code (e.g. RUR)>'
+    help = 'Fill RUR wallets for existing lorus and ughs, rates for ughs'
+    
+    def handle(self, args, **options):
+        currency = Currency.objects.get(code=args[0])
+        for org in Org.objects.filter(type__in=(Org.PROFILE_UGH, Org.PROFILE_LORU, )):
+            org.create_wallet_rate(currency)
