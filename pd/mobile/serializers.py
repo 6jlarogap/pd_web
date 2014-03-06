922e309d579d077cbea27f9ae08d1cb778d4f824
west <a.gurinovich89@gmail.com>
2014-03-06 19:01:59 +0300

    django-rest-framework in mobile api

---
+++ pd/mobile/serializers.py
@@ -0,0 +1,103 @@
+# coding=utf-8
+
+#from burials.models import Cemetery, Place, Area, Grave, Burial, AreaPhoto, GravePhoto
+from rest_framework import serializers
+
+class BaseSerializer(serializers.Serializer):	
+    pk = serializers.Field()    
+
+class CoordinatesSerializer(BaseSerializer):
+    lat = serializers.CharField(required=True)
+    lng = serializers.CharField(required=True)
+    angle_number = serializers.CharField(required=True)
+    
+    def restore_object(self, attrs, instance=None):
+        if instance is not None:
+            instance.pk = attrs.get('pk', instance.pk)
+            instance.lat = attrs.get('lat', instance.lat)
+            instance.lng = attrs.get('lng', instance.lng)
+            instance.angle_number = attrs.get('angle_number', instance.angle_number)
+            return instance
+        return CoordinatesModel(**attrs)
+        
+class CemeterySerializer(BaseSerializer):	
+    name = serializers.CharField(required=True)
+
+class CemeteryWithNestedObjectSerializer(CemeterySerializer):	
+    coordinates = CoordinatesSerializer(many=True)
+
+class AreaSerializer(BaseSerializer):
+    cemetery = BaseSerializer(required=True)
+    name = serializers.CharField(required=True)
+    
+class AreaWithNestedObjectSerializer(AreaSerializer):    
+    coordinates = CoordinatesSerializer(many=True)
+
+class RegionSerializer(serializers.Serializer):    
+    name = serializers.CharField(required=False)
+
+class CitySerializer(serializers.Serializer):    
+    name = serializers.CharField(required=False)
+    
+class StreetSerializer(serializers.Serializer):    
+    name = serializers.CharField(required=False)
+    
+class CountrySerializer(serializers.Serializer):    
+    name = serializers.CharField(required=False)
+    
+class LocationSerializer(serializers.Serializer):    
+    house = serializers.CharField(required=False)
+    block = serializers.CharField(required=False)
+    building = serializers.CharField(required=False)
+    flat = serializers.CharField(required=False)
+    country = CountrySerializer(required=False)
+    region = RegionSerializer(required=False)
+    city = CitySerializer(required=False)
+    street = StreetSerializer(required=False)
+
+class BasePersonSerializer(serializers.Serializer):    
+    last_name = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
+    middle_name = serializers.CharField(required=False)    
+    
+class AlivePersonSerializer(BasePersonSerializer):    
+    phones = serializers.CharField(required=False)
+    login_phone = serializers.CharField(required=False)
+    address = LocationSerializer(required=False)
+    
+class PlaceWithNestedObjectSerializer(BaseSerializer):    
+    cemetery = BaseSerializer(required=False)
+    area = BaseSerializer(required=True)    
+    row = serializers.CharField(required=False)
+    place = serializers.CharField(required=True)
+    oldplace = serializers.CharField(required=False)
+    place_width = serializers.CharField(required=False)
+    place_length = serializers.CharField(required=False)
+    dt_wrong_fio = serializers.DateTimeField(required=False)
+    dt_military = serializers.DateTimeField(required=False)
+    dt_size_violated = serializers.DateTimeField(required=False)
+    dt_unowned = serializers.DateTimeField(required=False)
+    dt_unindentified = serializers.DateTimeField(required=False)    
+    responsible = AlivePersonSerializer(required=False)
+    
+class GraveSerializer(BaseSerializer):    
+    place = BaseSerializer(required=True)    
+    grave_number = serializers.CharField(required=True)
+    is_military = serializers.CharField(required=False)
+    is_wrong_fio = serializers.CharField(required=False)
+    
+class BurialSerializer(BaseSerializer):    
+    grave = BaseSerializer(required=True)    
+    fact_date = serializers.CharField(required=False)
+    burial_container = serializers.CharField(required=False)
+    deadman = BasePersonSerializer(required=False)
+    
+class GravePhotoSerializer(BaseSerializer):  
+    grave = BaseSerializer(required=False)    
+    lat = serializers.CharField(required=False)
+    lng = serializers.CharField(required=False)
+
+class PlacePhotoSerializer(BaseSerializer):  
+    place = BaseSerializer(required=False)    
+    lat = serializers.CharField(required=False)
+    lng = serializers.CharField(required=False)
