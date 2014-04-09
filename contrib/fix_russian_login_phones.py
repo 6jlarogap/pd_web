d1e8f3b15fa513f93b9cc84f2ddf9e509e19885f
Eugene Suprun <suprune20@gmail.com>
2014-04-09 18:31:27 +0300

    contrib/fix_russian_login_phones.py

---
+++ contrib/fix_russian_login_phones.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# fix_russian_login_phones.py,
+#
+# Исправить номера телефонов для входа в кабинет
+# ответственного, при условии, что:
+#
+# !!!   все эти телефоны из России
+#
+# - телефон типа 9056416023 преобразуется в 79056416023
+# - телефон типа 89105161406 преобразуется в 79105161406
+#
+# Запуск из ./manage.py shell :
+# execfile('/path/to/fix_russian_login_phones.py')
+
+from persons.models import AlivePerson
+
+for person in AlivePerson.objects.filter(login_phone__isnull=False).order_by('login_phone').distinct('login_phone'):
+    login_phone = str(person.login_phone)
+    if len(login_phone) == 11 and login_phone.startswith('8'):
+        person.login_phone = '7' + login_phone[1:]
+        person.save()
+    elif len(login_phone) == 10 and login_phone.startswith('9'):
+        person.login_phone = '7' + login_phone
+        person.save()
