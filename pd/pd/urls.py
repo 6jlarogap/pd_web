c78d05734bfcfd009783b22f5bade70e3aa647ad
Eugene Suprun <suprune20@gmail.com>
2014-05-05 11:14:31 +0300

    code refactoring: rename /api/catalog/suppliers/categories to /api/catalog/categories

--- pd/pd/urls.py
+++ pd/pd/urls.py
@@ -51,7 +51,7 @@ router.register(r'^api/placesize', PlaceSizeViewSet)
 
 # Orders
 router.register(r'^api/catalog/suppliers', CatalogSuppliersViewSet)
-router.register(r'^api/catalog/suppliers/categories', ProductCategoryViewSet)
+router.register(r'^api/catalog/categories', ProductCategoryViewSet)
 router.register(r'^api/products', ProductsViewSet)
 router.register(r'^api/product', ProductInfoViewSet)
 router.register(r'^api/profile', ApiProfileViewSet)
