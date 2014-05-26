77560fd4da53752e3e51d076e1898c14b0dda8d5
Eugene Suprun <suprune20@gmail.com>
2014-05-26 18:20:26 +0300

    pd/restthumbnails_urls.py

---
+++ pd/pd/restthumbnails_urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.defaults import patterns, url
+
+from restthumbnails.defaults import URL_REGEX
+from pd.views import OurThumbnailView
+
+
+urlpatterns = patterns('',
+    url(regex=URL_REGEX,
+        view=OurThumbnailView.as_view(),
+        name="get_thumbnail"))
