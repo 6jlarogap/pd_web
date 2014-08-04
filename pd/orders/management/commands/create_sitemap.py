cef5b0a9bdf49cd0af16de8b8235602cc84c0047
Eugene Suprun <suprune20@gmail.com>
2014-08-04 11:43:10 +0300

    c.1152, профиль Поставщика, add suppliers in sitemap

--- pd/orders/management/commands/create_sitemap.py
+++ pd/orders/management/commands/create_sitemap.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import sys, os, codecs
+import sys, os, codecs, datetime
 
 from django.core.management.base import BaseCommand
 from django.template import loader, Context
 
 from django.conf import settings
 
-from orders.models import ProductStatus, ProductHistory
+from orders.models import Product, ProductStatus, ProductHistory
 from users.models import Org
 
 
@@ -25,15 +25,29 @@ class Command(BaseCommand):
         if not url.endswith('/'):
             url += '/'
 
+        published_statuses = (
+            ProductHistory.PRODUCT_OPERATION_PUBLISH,
+            ProductHistory.PRODUCT_OPERATION_UPDATE,
+        )
         catalog_org_pk = Org.get_catalog_org_pk()
         product_statuses = ProductStatus.objects.filter(
-            status__in=(ProductHistory.PRODUCT_OPERATION_PUBLISH, ProductHistory.PRODUCT_OPERATION_UPDATE, ),
+            status__in=published_statuses,
             ugh__pk=catalog_org_pk,
         )
+        
+        # Поставщики: дата их последней модификации является датой,
+        # когда они внесли свой последний опубликованный товар
+        suppliers = Product.objects.filter(
+            productstatus__status__in=published_statuses,
+            productstatus__ugh__pk=catalog_org_pk,
+        ).order_by('loru__slug','-productstatus__dt',). \
+        values('loru__slug', 'productstatus__dt',).\
+        distinct('loru__slug',)
 
         t = loader.get_template('sitemap.xml')
         xml = unicode(t.render(Context({
             'product_statuses': product_statuses,
+            'suppliers': suppliers,
             'url': url,
         })))
         
