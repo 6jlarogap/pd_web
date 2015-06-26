1cac8e1aa95f2a8512c4b6a0e2c76331c9f18cc1
Eugene Suprun <suprune20@gmail.com>
2015-06-26 16:45:40 +0300

    burial1 view fix

--- pd/burials/management/commands/create_burial_views.py
+++ pd/burials/management/commands/create_burial_views.py
@@ -10,42 +10,65 @@
 BURIAL_VIEWS_SQL = """
     CREATE OR REPLACE VIEW burials_burial1 AS
     SELECT *,
-    CASE WHEN account_number IS NULL
-        THEN ''
+
+    CASE
+        WHEN account_number IS NULL
+            THEN ''
         ELSE substring(account_number FROM '([^[:digit:]]*)[[:digit:]]*.*')
     END
-        AS account_number_s1,
-    CASE WHEN account_number IS NULL
-        THEN -1
-        ELSE to_number(CASE WHEN trim(both ' ' from substring(account_number FROM '[^[:digit:]]*([[:digit:]]*).*'))=''
-                            THEN '99999999999999999999'
-                    ELSE substring(account_number FROM '[^[:digit:]]*([[:digit:]]*).*')
-                END, '99999999999999999999')
+    AS account_number_s1,
+
+    CASE
+        WHEN account_number IS NULL
+            THEN -1
+        WHEN trim(both ' ' from account_number)=''
+            THEN -1
+        ELSE to_number(
+                CASE WHEN trim(both ' ' from substring(account_number FROM '[^[:digit:]]*([[:digit:]]*).*'))=''
+                        THEN '99999999999999999999'
+                     ELSE substring(account_number FROM '[^[:digit:]]*([[:digit:]]*).*')
+                END,
+                '99999999999999999999'
+             )
     END
-        AS account_number_s2,
-    CASE WHEN account_number IS NULL
-        THEN ''
+    AS account_number_s2,
+
+    CASE
+        WHEN account_number IS NULL
+            THEN ''
         ELSE substring(account_number FROM '[^[:digit:]]*[[:digit:]]*(.*)')
     END
-        AS account_number_s3,
-    CASE WHEN place_number IS NULL
-        THEN ''
+    AS account_number_s3,
+
+    CASE
+        WHEN place_number IS NULL
+            THEN ''
         ELSE substring(place_number FROM '([^[:digit:]]*)[[:digit:]]*.*')
     END
-        AS place_number_s1,
-    CASE WHEN place_number IS NULL
-        THEN -1
-        ELSE to_number(CASE WHEN trim(both ' ' from substring(place_number FROM '[^[:digit:]]*([[:digit:]]*).*'))=''
-                            THEN '99999999999999999999'
-                    ELSE substring(place_number FROM '[^[:digit:]]*([[:digit:]]*).*')
-                END, '99999999999999999999')
+    AS place_number_s1,
+
+    CASE
+        WHEN place_number IS NULL
+            THEN -1
+        WHEN trim(both ' ' from place_number)=''
+            THEN -1
+        ELSE to_number(
+                CASE WHEN trim(both ' ' from substring(place_number FROM '[^[:digit:]]*([[:digit:]]*).*'))=''
+                        THEN '99999999999999999999'
+                      ELSE substring(place_number FROM '[^[:digit:]]*([[:digit:]]*).*')
+                END,
+                '99999999999999999999'
+             )
     END
-        AS place_number_s2,
-    CASE WHEN place_number IS NULL
-        THEN ''
+    AS place_number_s2,
+
+    CASE
+        WHEN place_number IS NULL
+            THEN ''
         ELSE substring(place_number FROM '[^[:digit:]]*[[:digit:]]*(.*)')
     END
-        AS place_number_s3
+    AS place_number_s3
+
     FROM burials_burial;
 """
 
