147a1deb055a3956c4c282ead51eb937547d6447
Eugene Suprun <suprune20@gmail.com>
2016-02-18 17:45:43 +0300

    c.1345, Минимизировать эффект кэша javascript- сценариев

--- contrib/oms-local-copy/server/update_pd_django_only.sh
+++ contrib/oms-local-copy/server/update_pd_django_only.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
-cd /home/www-data/django/pd_web && \
-sudo -u www-data -H git pull && \
-cd /home/www-data/django/pd_web/pd && \
-sudo -u www-data ./manage.py migrate --noinput && \
-sudo -u www-data ./manage.py create_burial_views yes || true && \
-sudo -u www-data ./manage.py collectstatic --noinput && \
+PROJECT_='/home/www-data/django/pd_web'
+
+TTY_=`tty`
+cd "$PROJECT_/pd"
+sudo -u www-data -H git pull
+sudo -u www-data ./manage.py migrate --noinput
+sudo -u www-data ./manage.py create_burial_views yes
+
+sudo -u www-data ./manage.py collectstatic --noinput | \
+tee $TTY_ | \
+egrep -i '^[1-9][0-9]* static files? copied' > /dev/null && \
+echo 'Static file(s) changed. Touching static folder.' && \
+sudo -u www-data touch static
+
 sudo /etc/init.d/apache2 reload
 
-cd /home/chrooted/home/COMMON/django/pd_web && \
-sudo -u www-data -H git pull && \
-cd /home/chrooted/home/COMMON/django/pd_web/pd && \
+cd /home/chrooted/home/COMMON/django/pd_web
+sudo -u www-data -H git pull
+cd /home/chrooted/home/COMMON/django/pd_web/pd
 sudo -u www-data ./manage.py collectstatic --noinput
