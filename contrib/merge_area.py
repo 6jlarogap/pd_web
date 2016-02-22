c6b390da3f1c65703a9b49f100658b5091a073d1
Eugene Suprun <suprune20@gmail.com>
2016-02-22 12:39:03 +0300

    contrib/merge_area.py (2)

--- contrib/merge_area.py
+++ contrib/merge_area.py
@@ -5,8 +5,10 @@
 # с прежнего кладбища
 
 from django.db import models, transaction
+from django.contrib.contenttypes.models import ContentType
 
 from users.models import Org, Profile
+from logs.models import Log
 from burials.models import Burial, Cemetery, Area, Place
 
 @transaction.commit_on_success
@@ -45,6 +47,14 @@ def main():
         count +=1
         p.cemeteries.remove(cemetery_old)
     print "%s profiles with assigned cemetery_old updated" % count
+
+    print 'ct = ContentType.objects.get(app_label="burials", model="cemetery")'
+    print 'Log.objects.filter(ct=ct, obj_id=cemetery_old.pk).delete()'
+    ct = ContentType.objects.get(app_label="burials", model="cemetery")
+    log_recs = Log.objects.filter(ct=ct, obj_id=cemetery_old.pk)
+    log_recs_count = log_recs.count()
+    log_recs.delete()
+    print u'%s log records of cemetery_old deleted' % log_recs_count
     
     print u'Removing cemetery_old: %s' % cemetery_old
     cemetery_old.delete()
