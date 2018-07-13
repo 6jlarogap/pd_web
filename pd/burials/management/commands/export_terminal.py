4bffaf23247b2c552113e5b56c02f326d2590d8e
Eugene Suprun <suprune20@gmail.com>
2018-07-13 12:25:04 +0300

    export-terminal, 2018-07-13, добавлен новый формат вывода

--- pd/burials/management/commands/export_terminal.py
+++ pd/burials/management/commands/export_terminal.py
@@ -13,44 +13,82 @@ import pytils
 from django import db
 from django.core.management.base import BaseCommand
 from django.db.models.query_utils import Q
+from django.utils import translation
 
 from burials.models import Place, Grave, Burial, Cemetery
 
 # ПАРАМЕТРЫ ----------------------------------------------
-# ОМС, на которых кладбища с терминалами
-#
-UGH_PK = 2
 
 # Кладбища, на которых установлены терминалы. На одном из из минских кладбищ
 # может формироваться экспорт для нескольких "подкладбищ"
 
 CEMETERIES = (
     dict(
+        # Имя csv файла
+        #
         export='kolodischi',
-        csv_kwargs=dict(delimiter="\t"),
-        cemeteries=(u'Колодищи',),
+
+        # Список кладбищ/колумбариев, первичные ключи
+        #
+        cemeteries=[28, ],
+
+        # формат:
+        #   если False:
+        #       pk last_name first_name__middle_name date area row seat
+        #   если True:
+        #       pk last_name first_name__middle_name date cemetery area row_seat
+        #
+        # * разделитель полей: символ табуляции
+        # * seat: номер участка (места)
+        # * row_seat: 'ряд X, место Y' или 'место Y', где место может быть
+        #   'место' для колумбария или 'участок' для кладбищенского сектора
+        #
+        put_cemeteries=False,
    ),
+
     dict(
         export='voennoe',
-        csv_kwargs=dict(delimiter="\t"),
-        cemeteries=(u'Военное',),
+        cemeteries=[23, ],
+        put_cemeteries=False,
    ),
+
     dict(
         export='vostochnoe',
-        #csv_kwargs=dict(delimiter=" ", quotechar='"', quoting=csv.QUOTE_ALL),
-        csv_kwargs=dict(delimiter="\t"),
-        cemeteries=(
-            u'Восточное',
-            u'Уручье',
-            ),
+        cemeteries=[
+            2,              # Восточное
+            7,              # Уручье
+            ],
+        put_cemeteries=False,
    ),
+
+    dict(
+        export='vostochnoe_cemeteries',
+        cemeteries=[
+            2,              # Восточное
+            7,              # Уручье
+            37,             # Колумбарий Восточного
+            ],
+        put_cemeteries=True,
+   ),
+
     dict(
         export='zapadnoe',
-        csv_kwargs=dict(delimiter="\t"),
-        cemeteries=(u'Западное',),
+        cemeteries=[32, ],
    ),
 )
 
+# --------- ----------------------------------------------
+
+# ОМС, на котором кладбища с терминалами (fool-proof)
+#
+UGH_PK = 2
+
+# Формат csv файла
+#
+CSV_KWARGS = dict(delimiter="\t")
+
+# Символы, которые могут в фамилии, имени, отч., по ошибке
+#
 BAD_CHAR_RE = r'\\|\"|\''
 
 # --------- ----------------------------------------------
@@ -60,24 +98,26 @@ class Command(BaseCommand):
     help = "Form export csv files for terminal at some cemeteries"
 
     def handle(self, *args, **options):
+        translation.activate('ru')
+
         try:
             export_path=args[0]
         except IndexError:
             print "No export path specified as the parameter"
             quit()
         for cemetery_parms in CEMETERIES:
-            print "Processing bundle of cemeteries for %s" % cemetery_parms['export']
+            print "Processing bundle of cemeteries for %s.csv" % cemetery_parms['export']
             cemeteries = []
-            for cemetery_name in cemetery_parms['cemeteries']:
+            for cemetery_pk in cemetery_parms['cemeteries']:
                 try:
-                    cemetery = Cemetery.objects.get(ugh__pk=UGH_PK, name=cemetery_name)
+                    cemetery = Cemetery.objects.get(ugh__pk=UGH_PK, pk=cemetery_pk)
                     cemeteries.append(cemetery)
                 except Cemetery.DoesNotExist:
                     pass
             if not cemeteries:
                 print "    !!! No cemeteries in system found for bundle %s" % cemetery_parms['export']
                 continue
-            csv.register_dialect(cemetery_parms['export'], **cemetery_parms['csv_kwargs'])
+            csv.register_dialect(cemetery_parms['export'], **CSV_KWARGS)
             fname_export = os.path.join(export_path, "%s.csv" % cemetery_parms['export'])
             fname_export_partial = "%s.partial" % fname_export
             f = open(fname_export_partial, "w")
@@ -131,7 +171,7 @@ class Command(BaseCommand):
 
                     b_date = burial.fact_date
                     if b_date:
-                        date = "%02d.%02d.%04d" %(b_date.day, b_date.month, b_date.year)
+                        date = "%02d.%02d.%04d" % (b_date.day, b_date.month, b_date.year)
                     else:
                         date = u"-"
 
@@ -141,17 +181,48 @@ class Command(BaseCommand):
                     area = area.encode('cp1251')
 
                     row = re.sub(BAD_CHAR_RE, '', place.row)
-                    if not row:
-                        row = u"-"
-                    row= row.encode('cp1251')
-
                     seat = re.sub(BAD_CHAR_RE, '', place.place)
-                    if not seat:
-                        seat = u"-"
-                    seat = seat.encode('cp1251')
 
-                    columns = [pk, last_name, initials, date, area, row, seat]
+                    if cemetery_parms.get('put_cemeteries'):
+                        cemetery_name = burial.cemetery and burial.cemetery.name or ''
+                        if not cemetery_name:
+                            cemetery_name = u"-"
+                        cemetery_name_lower = cemetery_name.lower()
+                        if u'колумбари' not in cemetery_name_lower and \
+                           u'кладбищ' not in cemetery_name_lower:
+                            cemetery_name = u"Кладбище %s" % cemetery_name
+                        cemetery_name = cemetery_name.encode('cp1251')
+                        if row and seat:
+                            row_seat = u"ряд %s, %s %s " % (
+                                row,
+                                place.place_name(),
+                                seat
+                            )
+                        elif seat:
+                            row_seat = u"%s %s" % (
+                                place.place_name(),
+                                seat
+                            )
+                        elif row:
+                            row_seat = u"ряд %s" % (
+                                row,
+                            )
+                        else:
+                            row_seat = u"-"
+                        row_seat = row_seat.encode('cp1251')
+                        columns = [pk, last_name, initials, date, cemetery_name, area, row_seat]
+                    else:
+                        if not row:
+                            row = u"-"
+                        row = row.encode('cp1251')
+                        if not seat:
+                            seat = u"-"
+                        seat = seat.encode('cp1251')
+                        columns = [pk, last_name, initials, date, area, row, seat]
+
                     writer.writerow(columns)
                     db.reset_queries()
             f.close()
             os.rename(fname_export_partial, fname_export)
+
+        translation.deactivate()
