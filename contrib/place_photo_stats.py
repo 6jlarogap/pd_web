923f2ddffa09a7955dd86fabab575eda03b21fa6
Eugene Suprun <suprune20@gmail.com>
2018-10-04 16:48:45 +0300

    contrib/place_photo_stats.py (2)

--- contrib/place_photo_stats.py
+++ contrib/place_photo_stats.py
@@ -8,7 +8,7 @@
 
 UGH_PK = 2
 
-from burials.models import Cemetery, Place
+from burials.models import Cemetery, Place, PlacePhoto
 from django.db.models.query_utils import Q
 
 q = Q(ugh__pk=UGH_PK) & ~Q(name__icontains=u'колумбари')
@@ -17,10 +17,11 @@ for c in Cemetery.objects.filter(q).order_by('name'):
     places = Place.objects.filter(cemetery=c)
     count_places = places.distinct().count()
     count_places_with_photos = places.filter(placephoto__isnull=False).distinct().count()
-    percent = round(1.0*count_places_with_photos/count_places, 2)
-    print u"%s, всего участков: %s, участков с фото: %s, %s%%" % (
+    count_photos = PlacePhoto.objects.filter(place__cemetery=c).count()
+    # percent = round(1.0*count_places_with_photos/count_places, 2)
+    print u"%s, всего участков: %s, участков с фото: %s, всего фото участков: %s" % (
         c.name,
         count_places,
         count_places_with_photos,
-        percent,
+        count_photos,
     )
