fe41cfd0d75081a0cdd047ca11ba800e020c966f
Eugene Suprun <suprune20@gmail.com>
2018-12-17 14:38:26 +0300

    PersonID duplicate key error, try to fix

--- pd/burials/forms.py
+++ pd/burials/forms.py
@@ -768,7 +768,12 @@ class BurialForm(PartialFormMixin, ChildrenJSONMixin, LoggingFormMixin, SafeDele
                 if self.applicant_id_form.is_valid_data():
                     pid = self.applicant_id_form.save(commit=False)
                     pid.person = applicant
-                    pid.save()
+
+                    # Иногда возникает ошибка дублирования OneToOne ключа PersonID.person
+                    # Смоделировать ее не могу (Е.С.)
+                    # Посмотрим, поможет ли force_update= в pid.save()
+                    #
+                    pid.save(force_update=bool(pid.pk))
         else:
             self.safe_delete('applicant', self.instance)
             self.instance.flag_no_applicant_doc_required = False
