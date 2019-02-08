1e130f49f619cad8f5a70e9c7b66959ff7914153
Eugene Suprun <suprune20@gmail.com>
2019-02-08 15:39:42 +0300

    Django 1.8 -> 1.9 Upgrade (20)

--- pd/geo/serializers.py
+++ pd/geo/serializers.py
@@ -1,16 +1,9 @@
 # coding=utf-8
 
-from django.contrib.auth.models import Group, Permission
 from rest_framework import serializers
-from rest_framework.fields import Field
-
 
 from burials.models import Cemetery, Place, Area, BurialFiles
 from geo.models import Location, Country, Region, City, Street
-from django.shortcuts import get_object_or_404
-from django.core.exceptions import ObjectDoesNotExist, ValidationError
-from django.utils.encoding import smart_text
-
 
 class AddressLatLonMixin(serializers.ModelSerializer):
 
@@ -29,13 +22,11 @@ class CountrySerializer(serializers.ModelSerializer):
         model = Country
         fields = ('id','name', 'text') 
 
-
 class RegionSerializer(serializers.ModelSerializer):
     text = serializers.ReadOnlyField(source='name')
     class Meta:
         model = Region
         fields = ('id','name', 'text') # 'country',
-
 
 class CitySerializer(serializers.ModelSerializer):
     text = serializers.ReadOnlyField(source='name')
@@ -43,103 +34,17 @@ class CitySerializer(serializers.ModelSerializer):
         model = City
         fields = ('id','name', 'text') # 'region',
 
-
 class StreetSerializer(serializers.ModelSerializer):
     text = serializers.ReadOnlyField(source='name')
     class Meta:
         model = Street
         fields = ('id','name', 'text') #'city',
 
-
-# Nested tables fields serializer
-class CountrySlugRelatedField(serializers.SlugRelatedField):
-    def from_native(self, data):
-        if self.queryset is None:
-            raise Exception('Writable related fields must include a `queryset` argument')
-        
-        try:
-            name = self.parent.init_data.get(u"country"," ")
-            item, created = Country.objects.get_or_create(name=name, defaults={})
-        except (TypeError, ValueError):
-            msg = self.error_messages['invalid']
-            raise ValidationError(msg)
-        else:
-            return item
-
-
-class RegionSlugRelatedField(serializers.SlugRelatedField):
-    def from_native(self, data):
-        if self.queryset is None:
-            raise Exception('Writable related fields must include a `queryset` argument')
-        
-        try:
-            name = self.parent.init_data.get(u"country"," ")
-            country, created = Country.objects.get_or_create(name=name, defaults={})
-        except:
-            raise ValidationError(_(u'Страна не найдена'))
-
-        try:
-            name = self.parent.init_data.get(u"region"," ")
-            item, created = Region.objects.get_or_create(name=name, country=country)
-        except (TypeError, ValueError):
-            msg = self.error_messages['invalid']
-            raise ValidationError(msg)
-        else:
-            return item
-
-
-class CitySlugRelatedField(serializers.SlugRelatedField):    
-    def from_native(self, data):
-        if self.queryset is None:
-            raise Exception('Writable related fields must include a `queryset` argument')
-        
-        name = self.parent.init_data.get(u"country"," ")
-        country, created = Country.objects.get_or_create(name=name, defaults={})
-
-        name = self.parent.init_data.get(u"region"," ")
-        region, created = Region.objects.get_or_create(name=name, country=country)
-        
-
-        try:
-            name = self.parent.init_data.get(u"city"," ")
-            item, created = City.objects.get_or_create(name=name, region=region)
-        except (TypeError, ValueError):
-            msg = self.error_messages['invalid']
-            raise ValidationError(msg)
-        else:
-            return item
-
-
-class StreetSlugRelatedField(serializers.SlugRelatedField):
-    def from_native(self, data):
-        if self.queryset is None:
-            raise Exception('Writable related fields must include a `queryset` argument')
-        
-        name = self.parent.init_data.get(u"country"," ")
-        country, created = Country.objects.get_or_create(name=name, defaults={})
-
-        name = self.parent.init_data.get(u"region"," ")
-        region, created = Region.objects.get_or_create(name=name, country=country)
-
-        name = self.parent.init_data.get(u"city"," ")
-        city, created = City.objects.get_or_create(name=name, region=region)
-
-        try:
-            name = self.parent.init_data.get(u"street"," ")
-            item, created = Street.objects.get_or_create(name=data, city=city)
-        except (TypeError, ValueError):
-            msg = self.error_messages['invalid']
-            raise ValidationError(msg)
-        else:
-            return item
-# EOF Nested tables fields serializer
-
-
 class LocationSerializer(serializers.ModelSerializer):
-    country = CountrySlugRelatedField(many=False, required=False, read_only=True, slug_field='name')
-    region  = RegionSlugRelatedField(many=False, required=False,read_only=True, slug_field='name')
-    city    = CitySlugRelatedField(many=False, required=False, read_only=True, slug_field='name')
-    street  = StreetSlugRelatedField(many=False, required=False, read_only=True, slug_field='name')
+    country = serializers.SlugRelatedField(many=False, required=False, read_only=True, slug_field='name')
+    region  = serializers.SlugRelatedField(many=False, required=False, read_only=True, slug_field='name')
+    city    = serializers.SlugRelatedField(many=False, required=False, read_only=True, slug_field='name')
+    street  = serializers.SlugRelatedField(many=False, required=False, read_only=True, slug_field='name')
     class Meta:
         model = Location
         fields = ('id','country', 'region', 'city', 'street', \
@@ -162,4 +67,3 @@ class LocationDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ('id', 'post_index', 'house', 'block', 'building', 'flat', 'gps_x', 'gps_y', 'info' ) 
-
