defa4df8afd21cb68c27c6951c29323fcfb7586c
Eugene Suprun <suprune20@gmail.com>
2022-12-14 18:47:34 +0300

    check valid place length, width (4)

--- pd/burials/models.py
+++ pd/burials/models.py
@@ -446,9 +446,9 @@ class Place(SafeDeleteMixin, GeoPointModel, BaseModelManualDtCreated):
                 precision=Place._meta.get_field('geohash').max_length,
             )
         MAX_LEN = 999
-        if self.place_length and float(self.place_length) > MAX_LEN:
+        if self.place_length and abs(float(self.place_length)) > MAX_LEN:
             self.place_length = MAX_LEN
-        if self.place_width and float(self.place_width) > MAX_LEN:
+        if self.place_width and abs(float(self.place_width)) > MAX_LEN:
             self.place_width = MAX_LEN
 
         return super(Place, self).save(*args, **kwargs)
